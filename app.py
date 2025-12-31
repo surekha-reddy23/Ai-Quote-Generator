e53f5966eb2cfc0be498dd3d51f4bdd2f5b08182
surekha-reddy23 <reddysurekha2323@gmail.com>
2025-12-31 10:37:29 +0530

    Refactor app to use dotenv and MySQL connection
    Updated the application to load environment variables and connect to a MySQL database. Changed the route to '/quote' and modified the quote generation logic to return multiple quotes.

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template, request
 from google import genai
+from dotenv import load_dotenv
+import mysql.connector
+load_dotenv()
+import os
+
 app = Flask(__name__)
 
-client = genai.Client(api_key="AIzaSyAj2D1bdvrdRwQsi1ZRQpWyG7oXvcd9eT4")
-@app.route("/",methods=["GET","POST"])
+client = genai.Client(api_key=os.getenv("api"))
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="mysql",
+        database="python_db"
+    )
+@app.route("/quote",methods=["GET","POST"])
 def index():
-    quote = ""
+    quote = []
     if request.method == "POST":
         topic = request.form["topic"]
-        response = client.models.generate_content(
-            model="models/gemini-flash-latest",
-            contents=f"Generate a short inspirational quote about {topic}"
+        prompt = f"""
+        Generate 5 short inspirational quotes about {topic}.
 
+        """
+        response = client .models.generate_content(
+            model="models/gemini-flash-latest",
+            contents=prompt
         )
-        quote = response.text
+        quote = response.text.strip().split("\n")
     return render_template("index.html",quote=quote)
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+   
